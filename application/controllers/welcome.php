8cd10448a84d4dc448687a0c3057403afc843b89
Steven Lu <tacticalazn@gmail.com>
2013-01-12 17:58:53 -0500

    Modified some styles, added sessions.

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -3,15 +3,26 @@
 class Welcome extends CI_Controller
 {
 
+	/**
+	 * index()
+	 * Does a lot of stuff.
+	 */
 	function index()
 	{
 		$this->load->view('include/header');
 
-		$code = $this->input->get('code');
-		if (!empty($code))
+		if ($code = $this->input->get('code'))
 		{
 			$this->load->model('github_model');
-			$access_token = $this->github_model->get_access_token($code);
+
+			$this->load->library('session');
+			if (!$access_token = $this->session->userdata('access_token'))
+				$access_token = $this->github_model->get_access_token($code);
+
+			if (!$access_token)
+				$this->load->view('welcome', array('error' => true));
+
+			$this->session->set_userdata('access_token', $access_token);
 			// what to do with this access token?
 		}
 		else
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,7 +1,10 @@
 <div class="container">
+	<? if (isset($error)): ?>
+	<div class="alert alert-error"><strong>Oh no!</strong> Something went wrong during authentication. Maybe trying again will help?</div>
+	<? endif; ?>
 	<div class="hero-unit">
-		<h2>Your Commits Under Review</h2>
+		<h2>Commits Under Review</h2>
 		<p>We look at your Github commits for the past year and see how well you've done.</p>
-		<a href="<?= github_oauth_url() ?>" class="btn"><img src="https://raw.github.com/github/media/master/octocats/blacktocat-32.png" alt="github logo" /> Login using Github</a>
+		<a href="<?= github_oauth_url() ?>" class="btn"><img id="github-logo" src="https://raw.github.com/github/media/master/octocats/blacktocat-32.png" alt="github logo" /> Login using Github</a>
 	</div>
 </div>
